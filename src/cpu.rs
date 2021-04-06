aedcf77456ae72be3b3dc99c1d20c4d36ac9070a
Louise <louise@zanier.org>
2021-04-06 15:25:14 +0200

    Better Debug printing for the CPU

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,9 +5,55 @@ use std::io::Read;
 const DRAM_SIZE: usize = 1024 * 1024 * 128;
 
 #[derive(Default)]
+struct IRegisters {
+    regs: [u64; 32]
+}
+
+impl IRegisters {
+    pub fn read_reg(&self, reg: u32) -> u64 {
+        if reg == 0 {
+            0
+        } else {
+            self.regs[reg as usize]
+        }
+    }
+
+    pub fn write_reg(&mut self, reg: u32, value: u64) {
+        if reg != 0 {
+            self.regs[reg as usize] = value
+        }
+    }
+}
+
+impl std::fmt::Debug for IRegisters {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let abi = [
+            "zero", " ra ", " sp ", " gp ", " tp ", " t0 ", " t1 ", " t2 ", " s0 ", " s1 ", " a0 ",
+            " a1 ", " a2 ", " a3 ", " a4 ", " a5 ", " a6 ", " a7 ", " s2 ", " s3 ", " s4 ", " s5 ",
+            " s6 ", " s7 ", " s8 ", " s9 ", " s10", " s11", " t3 ", " t4 ", " t5 ", " t6 ",
+        ];
+
+        let mut output = String::new();
+        for i in (0..32).step_by(4) {
+            output = format!("{}\n{}",
+                output,
+                format!("x{:02}({})={:016x} x{:02}({})={:016x} x{:02}({})={:016x} x{:02}({})={:016x}",
+                        i, abi[i], self.read_reg(i as u32),
+                        i + 1, abi[i + 1], self.read_reg(i as u32 + 1),
+                        i + 2, abi[i + 2], self.read_reg(i as u32 + 2),
+                        i + 3, abi[i + 3], self.read_reg(i as u32 + 3),
+                )
+            )
+        }
+
+        write!(f, "{}", output.trim_start())
+    }
+}
+
+#[derive(Default)]
 pub struct CPU {
     pc: u64,
-    regs: [u64; 32],
+    iregs: IRegisters,
     bus: Bus,
     pub halt: bool
 }
@@ -16,7 +62,7 @@ impl CPU {
     pub fn new() -> Self {
         Self {
             pc: 0x80000000,
-            regs: [0; 32],
+            iregs: Default::default(),
             bus: Bus::new(DRAM_SIZE),
             halt: false
         }
@@ -51,23 +97,23 @@ impl CPU {
             0x03 => {
                 // RV32/64I load functions
                 let imm = ((instr as i32 as i64) >> 20) as u64;
-                let addr = self.read_reg(rs1).wrapping_add(imm);
+                let addr = self.iregs.read_reg(rs1).wrapping_add(imm);
 
                 match funct3 {
                     // LB
-                    0x0 => { let value = self.bus.load8(addr) as i8 as i64 as u64; self.write_reg(rd, value) }
+                    0x0 => { let value = self.bus.load8(addr) as i8 as i64 as u64; self.iregs.write_reg(rd, value) }
                     // LH
-                    0x1 => { let value = self.bus.load16(addr) as i16 as i64 as u64; self.write_reg(rd, value) }
+                    0x1 => { let value = self.bus.load16(addr) as i16 as i64 as u64; self.iregs.write_reg(rd, value) }
                     // LW
-                    0x2 => { let value = self.bus.load32(addr) as i32 as i64 as u64; self.write_reg(rd, value) }
+                    0x2 => { let value = self.bus.load32(addr) as i32 as i64 as u64; self.iregs.write_reg(rd, value) }
                     // LD
-                    0x3 => { let value = self.bus.load64(addr); self.write_reg(rd, value) }
+                    0x3 => { let value = self.bus.load64(addr); self.iregs.write_reg(rd, value) }
                     // LBU
-                    0x4 => { let value = self.bus.load8(addr) as u64; self.write_reg(rd, value) }
+                    0x4 => { let value = self.bus.load8(addr) as u64; self.iregs.write_reg(rd, value) }
                     // LHU
-                    0x5 => { let value = self.bus.load16(addr) as u64; self.write_reg(rd, value) }
+                    0x5 => { let value = self.bus.load16(addr) as u64; self.iregs.write_reg(rd, value) }
                     // LWU
-                    0x6 => { let value = self.bus.load32(addr) as u64; self.write_reg(rd, value) }
+                    0x6 => { let value = self.bus.load32(addr) as u64; self.iregs.write_reg(rd, value) }
                     _ => unimplemented!("funct3 not yet implemented ({:2x}, {:1x})", opcode, funct3)
                 }
             },
@@ -79,23 +125,23 @@ impl CPU {
 
                 match (funct3, funct7) {
                     // ADDI
-                    (0x0, _) => { self.write_reg(rd, self.read_reg(rs1).wrapping_add(imm)) }
+                    (0x0, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1).wrapping_add(imm)) }
                     // SLLI
-                    (0x1, _) => { self.write_reg(rd, self.read_reg(rs1) << shamt) }
+                    (0x1, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) << shamt) }
                     // SLTI
-                    (0x2, _) => { self.write_reg(rd, ((self.read_reg(rs1) as i64) < imm as i64) as u64) }
+                    (0x2, _) => { self.iregs.write_reg(rd, ((self.iregs.read_reg(rs1) as i64) < imm as i64) as u64) }
                     // SLTIU
-                    (0x3, _) => { self.write_reg(rd, (self.read_reg(rs1) < imm) as u64)},
+                    (0x3, _) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) < imm) as u64)},
                     // XORI
-                    (0x4, _) => { self.write_reg(rd, self.read_reg(rs1) ^ imm)}
+                    (0x4, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) ^ imm)}
                     // SRLI
-                    (0x5, 0x0) => { self.write_reg(rd, self.read_reg(rs1) >> shamt) }
+                    (0x5, 0x0) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) >> shamt) }
                     // SRAI
-                    (0x5, 0x1) => { self.write_reg(rd, (self.read_reg(rs1) as i64 >> shamt) as u64) }
+                    (0x5, 0x1) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) as i64 >> shamt) as u64) }
                     // ORI
-                    (0x6, _) => { self.write_reg(rd, self.read_reg(rs1) | imm) }
+                    (0x6, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) | imm) }
                     // ANDI
-                    (0x7, _) => { self.write_reg(rd, self.read_reg(rs1) & imm) }
+                    (0x7, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) & imm) }
                     _ => unimplemented!("funct3 or funct7 not implemented! (instr: {:08x})", instr)
                 }
             },
@@ -104,7 +150,7 @@ impl CPU {
                 let imm = (instr & 0xFFFFF000) as i32 as i64 as u64;
                 let value = self.pc.wrapping_add(imm).wrapping_sub(4);
 
-                self.write_reg(rd, value);
+                self.iregs.write_reg(rd, value);
             }
             0x1B => {
                 // RV64I immediates
@@ -114,13 +160,13 @@ impl CPU {
 
                 match (funct3, funct7) {
                     // ADDIW
-                    (0x0, _) => { self.write_reg(rd, self.read_reg(rs1).wrapping_add(imm as i64 as i32 as u64))}
+                    (0x0, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1).wrapping_add(imm as i64 as i32 as u64))}
                     // SLLIW
-                    (0x1, _) => { self.write_reg(rd, (self.read_reg(rs1) << shamt) as i32 as u64) }
+                    (0x1, _) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) << shamt) as i32 as u64) }
                     // SRLIW
-                    (0x5, 0x0) => { self.write_reg(rd, (self.read_reg(rs1) >> shamt) as i32 as u32 as u64) }
+                    (0x5, 0x0) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) >> shamt) as i32 as u32 as u64) }
                     // SRAIW
-                    (0x5, 0x1) => { self.write_reg(rd, ((self.read_reg(rs1) as i32) >> shamt) as u32 as u64) }
+                    (0x5, 0x1) => { self.iregs.write_reg(rd, ((self.iregs.read_reg(rs1) as i32) >> shamt) as u32 as u64) }
                     _ => unimplemented!("Not implemented yet")
                 }
             },
@@ -128,8 +174,8 @@ impl CPU {
                 // RV32/64I store instructions
                 let imm = ((((instr & 0xfe000000) as i32 as i64) >> 20) ) as u64 | ((instr >> 7) & 0x1F) as u64;
                 let rs2 = (instr >> 20) & 0x1F;
-                let addr = self.read_reg(rs1).wrapping_add(imm);
-                let value = self.read_reg(rs2);
+                let addr = self.iregs.read_reg(rs1).wrapping_add(imm);
+                let value = self.iregs.read_reg(rs2);
 
                 match funct3 {
                     // SB
@@ -146,7 +192,7 @@ impl CPU {
             0x37 => {
                 // LUI
                 let imm = (instr & 0xFFFFF000) as i32 as i64 as u64;
-                self.write_reg(rd, imm as i32 as i64 as u64);
+                self.iregs.write_reg(rd, imm as i32 as i64 as u64);
             }
             0x33 => {
                 // RV32/64I Register-to-register functions
@@ -155,25 +201,25 @@ impl CPU {
 
                 match (funct3, funct7) {
                     // ADD
-                    (0x0, 0x0) => { self.write_reg(rd, self.read_reg(rs1).wrapping_add(self.read_reg(rs2))) }
+                    (0x0, 0x0) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1).wrapping_add(self.iregs.read_reg(rs2))) }
                     // SUB
-                    (0x0, 0x1) => { self.write_reg(rd, self.read_reg(rs1).wrapping_sub(self.read_reg(rs2))) }
+                    (0x0, 0x1) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1).wrapping_sub(self.iregs.read_reg(rs2))) }
                     // SLL
-                    (0x1, _) => { self.write_reg(rd, self.read_reg(rs1) << (self.read_reg(rs2) & 0x3F)) }
+                    (0x1, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) << (self.iregs.read_reg(rs2) & 0x3F)) }
                     // SLT
-                    (0x2, _) => { self.write_reg(rd, ((self.read_reg(rs1) as i64) < (self.read_reg(rs2) as i64)) as u64)}
+                    (0x2, _) => { self.iregs.write_reg(rd, ((self.iregs.read_reg(rs1) as i64) < (self.iregs.read_reg(rs2) as i64)) as u64)}
                     // SLTU
-                    (0x3, _) => { self.write_reg(rd, (self.read_reg(rs1) < self.read_reg(rs2)) as u64) }
+                    (0x3, _) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) < self.iregs.read_reg(rs2)) as u64) }
                     // XOR
-                    (0x4, _) => { self.write_reg(rd, self.read_reg(rs1) ^ self.read_reg(rs2)) }
+                    (0x4, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) ^ self.iregs.read_reg(rs2)) }
                     // SRL
-                    (0x5, 0x0) => { self.write_reg(rd, self.read_reg(rs1) >> (self.read_reg(rs2) & 0x3F)) }
+                    (0x5, 0x0) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) >> (self.iregs.read_reg(rs2) & 0x3F)) }
                     // SRA
-                    (0x5, 0x1) => { self.write_reg(rd, ((self.read_reg(rs1) as i64) >> (self.read_reg(rs2) & 0x3F)) as u64) }
+                    (0x5, 0x1) => { self.iregs.write_reg(rd, ((self.iregs.read_reg(rs1) as i64) >> (self.iregs.read_reg(rs2) & 0x3F)) as u64) }
                     // OR
-                    (0x6, _) => { self.write_reg(rd, self.read_reg(rs1) | self.read_reg(rs2)) }
+                    (0x6, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) | self.iregs.read_reg(rs2)) }
                     // AND
-                    (0x7, _) => { self.write_reg(rd, self.read_reg(rs1) & self.read_reg(rs2)) }
+                    (0x7, _) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1) & self.iregs.read_reg(rs2)) }
                     _ => unimplemented!("funct3 or funct7 not implemented! (instr: {:08x}", instr)
                 }
             },
@@ -184,15 +230,15 @@ impl CPU {
 
                 match (funct3, funct7) {
                     // ADD
-                    (0x0, 0x0) => { self.write_reg(rd, self.read_reg(rs1).wrapping_add(self.read_reg(rs2) as i64 as i32 as u64)) }
+                    (0x0, 0x0) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1).wrapping_add(self.iregs.read_reg(rs2) as i64 as i32 as u64)) }
                     // SUB
-                    (0x0, 0x1) => { self.write_reg(rd, self.read_reg(rs1).wrapping_sub(self.read_reg(rs2) as i64 as i32 as u64)) }
+                    (0x0, 0x1) => { self.iregs.write_reg(rd, self.iregs.read_reg(rs1).wrapping_sub(self.iregs.read_reg(rs2) as i64 as i32 as u64)) }
                     // SLLW
-                    (0x1, _) => { self.write_reg(rd, (self.read_reg(rs1) << (self.read_reg(rs2) & 0x1f)) as i32 as u64) }
+                    (0x1, _) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) << (self.iregs.read_reg(rs2) & 0x1f)) as i32 as u64) }
                     // SRLW
-                    (0x5, 0x0) => { self.write_reg(rd, (self.read_reg(rs1) >> (self.read_reg(rs2) & 0x1f)) as i32 as u32 as u64) }
+                    (0x5, 0x0) => { self.iregs.write_reg(rd, (self.iregs.read_reg(rs1) >> (self.iregs.read_reg(rs2) & 0x1f)) as i32 as u32 as u64) }
                     // SRAW
-                    (0x5, 0x1) => { self.write_reg(rd, ((self.read_reg(rs1) as i32) >> (self.read_reg(rs2) & 0x1f)) as u32 as u64) }
+                    (0x5, 0x1) => { self.iregs.write_reg(rd, ((self.iregs.read_reg(rs1) as i32) >> (self.iregs.read_reg(rs2) & 0x1f)) as u32 as u64) }
                     _ => unimplemented!("Not implemented yet")
                 }
             },
@@ -207,17 +253,17 @@ impl CPU {
 
                 match funct3 {
                     // BEQ
-                    0x0 => { if self.read_reg(rs1) == self.read_reg(rs2) { self.pc = addr; } }
+                    0x0 => { if self.iregs.read_reg(rs1) == self.iregs.read_reg(rs2) { self.pc = addr; } }
                     // BNE
-                    0x1 => { if self.read_reg(rs1) != self.read_reg(rs2) { self.pc = addr; } }
+                    0x1 => { if self.iregs.read_reg(rs1) != self.iregs.read_reg(rs2) { self.pc = addr; } }
                     // BLT
-                    0x4 => { if (self.read_reg(rs1) as i64) < self.read_reg(rs2) as i64 { self.pc = addr; } }
+                    0x4 => { if (self.iregs.read_reg(rs1) as i64) < self.iregs.read_reg(rs2) as i64 { self.pc = addr; } }
                     // BGE
-                    0x5 => { if self.read_reg(rs1) as i64 >= self.read_reg(rs2) as i64 { self.pc = addr; } }
+                    0x5 => { if self.iregs.read_reg(rs1) as i64 >= self.iregs.read_reg(rs2) as i64 { self.pc = addr; } }
                     // BLTU 
-                    0x6 => { if self.read_reg(rs1) < self.read_reg(rs2) { self.pc = addr; } }
+                    0x6 => { if self.iregs.read_reg(rs1) < self.iregs.read_reg(rs2) { self.pc = addr; } }
                     // BGEU
-                    0x7 => { if self.read_reg(rs1) >= self.read_reg(rs2) { self.pc = addr; } }
+                    0x7 => { if self.iregs.read_reg(rs1) >= self.iregs.read_reg(rs2) { self.pc = addr; } }
                     _ => panic!("Bad instruction!")
                 }
             }
@@ -225,10 +271,10 @@ impl CPU {
                 // JALR
                 let offset = (instr as i32) >> 20;
                 let old_pc = self.pc;
-                let target = (self.read_reg(rs1).wrapping_add(offset as u32 as u64)) & !1;
+                let target = (self.iregs.read_reg(rs1).wrapping_add(offset as u32 as u64)) & !1;
 
                 self.pc = target;
-                self.write_reg(rd, old_pc as u64);
+                self.iregs.write_reg(rd, old_pc as u64);
             },
             0x6F => {
                 // JAL
@@ -238,7 +284,7 @@ impl CPU {
                 ((instr >> 9) & 0x800) as u64 |
                 ((instr >> 20) & 0x7fe) as u64;
 
-                self.write_reg(rd, self.pc);
+                self.iregs.write_reg(rd, self.pc);
                 self.pc = self.pc.wrapping_add(offset).wrapping_sub(4);
             },
             _ => unimplemented!("Opcode not implemented! ({:02x})", opcode)
@@ -249,43 +295,13 @@ impl CPU {
         let instr = self.fetch();
         self.execute(instr);
     }
-
-    pub fn read_reg(&self, reg: u32) -> u64 {
-        if reg == 0 {
-            0
-        } else {
-            self.regs[reg as usize]
-        }
-    }
-
-    pub fn write_reg(&mut self, reg: u32, value: u64) {
-        if reg != 0 {
-            self.regs[reg as usize] = value
-        }
-    }
 }
 
 impl std::fmt::Debug for CPU {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let abi = [
-            "zero", " ra ", " sp ", " gp ", " tp ", " t0 ", " t1 ", " t2 ", " s0 ", " s1 ", " a0 ",
-            " a1 ", " a2 ", " a3 ", " a4 ", " a5 ", " a6 ", " a7 ", " s2 ", " s3 ", " s4 ", " s5 ",
-            " s6 ", " s7 ", " s8 ", " s9 ", " s10", " s11", " t3 ", " t4 ", " t5 ", " t6 ",
-        ];
-
-        let mut output = String::new();
-        for i in (0..32).step_by(4) {
-            output = format!("{}\n{}",
-                output,
-                format!("x{:02}({})={:016x} x{:02}({})={:016x} x{:02}({})={:016x} x{:02}({})={:016x}",
-                        i, abi[i], self.read_reg(i as u32),
-                        i + 1, abi[i + 1], self.read_reg(i as u32 + 1),
-                        i + 2, abi[i + 2], self.read_reg(i as u32 + 2),
-                        i + 3, abi[i + 3], self.read_reg(i as u32 + 3),
-                )
-            )
-        }
-
-        write!(f, "{}", output)
+        write!(f, 
+            "CPU {{\n\
+            PC: {:016x}, Bus: {:?}\n\
+            {:?}", self.pc, self.bus, self.iregs)
     }
 }
